1f2d85dafa6234a5fb53dbed7bff1027b47fccc7
Tushar Chandak <tusharchandak2004@gmail.com>
2023-09-04 22:38:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-// Get a reference to the button and result div
-const checkLocationButton = document.getElementById("checkLocation");
+// Coordinates and radii for different hostels
+const hostelData = {
+    university: { lat: 12.870395999094573, lng: 80.22424001419108, radius: 45.2 },
+    mgr: { lat: 12.871296988176596, lng: 80.21899597808388, radius: 70 },
+    gopalakrishnan: { lat: 12.872120382563974, lng: 80.21933513902287, radius: 50 }
+};
+
+// Get references to the HTML elements
+const checkCustomLocationButton = document.getElementById("checkCustomLocation");
+const autoLocationButton = document.getElementById("autoLocation");
+const userLocationDiv = document.getElementById("userLocation");
 const resultDiv = document.getElementById("result");
-
-//St.Anthony Illam Boys Hostel coordinates (latitude and longitude)
-const universityLat = 12.870395999094573;
-const universityLng = 80.22424001419108
-;
-
-// Radius in meters
-const radius = 45.2;
+const attendancePopup = document.getElementById("attendancePopup");
+const giveAttendanceButton = document.getElementById("giveAttendance");
+const attendanceSuccess = document.getElementById("attendanceSuccess");
 
 // Function to calculate the distance between two points using the Haversine formula
 function calculateDistance(lat1, lon1, lat2, lon2) {
@@ -23,65 +27,92 @@ function calculateDistance(lat1, lon1, lat2, lon2) {
     return distance;
 }
 
-// Function to check the user's location
-function checkLocation() {
+// Function to get the user's location automatically
+function getAutoLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             const userLat = position.coords.latitude;
             const userLng = position.coords.longitude;
-            const distance = calculateDistance(userLat, userLng, universityLat, universityLng);
+            
+            // Update the latitude and longitude input fields
+            document.getElementById("latitude").value = userLat.toFixed(6);
+            document.getElementById("longitude").value = userLng.toFixed(6);
 
-            // Display the user's location
-            const userLocationDiv = document.getElementById("userLocation");
             userLocationDiv.innerHTML = `Your Location: Latitude ${userLat.toFixed(6)}, Longitude ${userLng.toFixed(6)}`;
-
-            if (distance <= radius) {
-                resultDiv.innerHTML = "You are in St.Anthony Illam Boys Hostel.";
-                resultDiv.classList.add("popup"); // Add the popup class
-            } else {
-                resultDiv.innerHTML = "You are not in St.Anthony Illam Boys Hostel.";
-                resultDiv.classList.remove("popup"); // Remove the popup class if not near
-            }
         });
     } else {
         resultDiv.innerHTML = "Geolocation is not supported by your browser.";
     }
 }
 
+// Function to show the attendance popup
+function showAttendancePopup() {
+    attendancePopup.style.display = "block";
+}
 
-// Add a click event listener to the button
-checkLocationButton.addEventListener("click", checkLocation);
+// Function to give attendance
+function giveHostelAttendance() {
+    attendanceSuccess.style.display = "block";
+    giveAttendanceButton.disabled = true;
+}
 
-
-// Get references to the custom location input fields and button
-const latitudeInput = document.getElementById("latitude");
-const longitudeInput = document.getElementById("longitude");
-const checkCustomLocationButton = document.getElementById("checkCustomLocation");
-
-// Function to check a custom location
+// Function to check proximity to a selected hostel
 function checkCustomLocation() {
-    const userLat = parseFloat(latitudeInput.value);
-    const userLng = parseFloat(longitudeInput.value);
+    const selectedHostel = document.getElementById("hostel").value;
+    const userLat = parseFloat(document.getElementById("latitude").value);
+    const userLng = parseFloat(document.getElementById("longitude").value);
 
     if (isNaN(userLat) || isNaN(userLng)) {
         resultDiv.innerHTML = "Please enter valid coordinates.";
         resultDiv.classList.remove("popup");
     } else {
-        const distance = calculateDistance(userLat, userLng, universityLat, universityLng);
+        const hostel = hostelData[selectedHostel];
+        const distance = calculateDistance(userLat, userLng, hostel.lat, hostel.lng);
 
         // Display the user's custom location
-        const userLocationDiv = document.getElementById("userLocation");
         userLocationDiv.innerHTML = `Your Custom Location: Latitude ${userLat.toFixed(6)}, Longitude ${userLng.toFixed(6)}`;
 
-        if (distance <= radius) {
-            resultDiv.innerHTML = "You are near St.Anthony Illam Boys Hostel.";
+        if (distance <= hostel.radius) {
+            resultDiv.innerHTML = `You are near ${selectedHostel} Boys Hostel.`;
             resultDiv.classList.add("popup");
+            
+            // Show attendance popup
+            showAttendancePopup();
         } else {
-            resultDiv.innerHTML = "You are not near St.Anthony Illam Boys Hostel.";
+            resultDiv.innerHTML = `You are not near ${selectedHostel} Boys Hostel.`;
             resultDiv.classList.remove("popup");
         }
     }
 }
 
-// Add a click event listener to the custom location button
+// Add click event listeners to the buttons
+autoLocationButton.addEventListener("click", getAutoLocation);
 checkCustomLocationButton.addEventListener("click", checkCustomLocation);
+giveAttendanceButton.addEventListener("click", giveHostelAttendance);
+
+// ...
+
+// Function to give attendance
+function giveHostelAttendance() {
+    const selectedHostel = document.getElementById("hostel").value;
+    const userLat = parseFloat(document.getElementById("latitude").value);
+    const userLng = parseFloat(document.getElementById("longitude").value);
+
+    if (isNaN(userLat) || isNaN(userLng)) {
+        resultDiv.innerHTML = "Please enter valid coordinates.";
+        resultDiv.classList.remove("popup");
+    } else {
+        const hostel = hostelData[selectedHostel];
+        const distance = calculateDistance(userLat, userLng, hostel.lat, hostel.lng);
+
+        if (distance <= hostel.radius) {
+            attendanceSuccess.style.display = "block";
+            giveAttendanceButton.disabled = true;
+        } else {
+            resultDiv.innerHTML = "You are not near the selected hostel. Attendance cannot be given.";
+            resultDiv.classList.remove("popup");
+        }
+    }
+}
+
+// ...
